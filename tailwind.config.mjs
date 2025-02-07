b9e50af8170d2a0a19a166579b40c986ac4cbbec
Tiburso <manuelmariamc@gmail.com>
2025-02-07 13:18:47 +0100

    css globals now work

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       container: {
@@ -11,6 +11,9 @@ module.exports = {
           lg: "4rem",
           xl: "5rem",
         },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
       },
     },
   },
